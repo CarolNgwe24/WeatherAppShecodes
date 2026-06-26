5344611a6de15cf5b4edda70d8a579fcea679118
CarolNgwe24 <145802881+CarolNgwe24@users.noreply.github.com>
2026-06-26 07:56:38 +0200

    index.js

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ let apiKey = "1e1cb74ecefd5a7o05tdff4f07af3619";
 let temperatureElement = document.querySelector("#temperature");
 let currentTemperature = null;
 
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  return days[date.getDay()];
+}
+
 function showWeather(response) {
   currentTemperature = response.data.temperature.current;
 
@@ -18,10 +33,35 @@ function showWeather(response) {
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
+function showForecast(response) {
+  let forecastDays = response.data.daily;
+
+  forecastDays.forEach(function (forecastDay, index) {
+    if (index < 7) {
+      document.querySelector(`#day-${index}`).innerHTML = formatDay(
+        forecastDay.time
+      );
+
+      let forecastIcon = document.querySelector(`#icon-${index}`);
+      forecastIcon.setAttribute("src", forecastDay.condition.icon_url);
+      forecastIcon.setAttribute("alt", forecastDay.condition.description);
+
+      document.querySelector(`#temp-max-${index}`).innerHTML = `${Math.round(
+        forecastDay.temperature.maximum
+      )}°`;
+      document.querySelector(`#temp-min-${index}`).innerHTML = `${Math.round(
+        forecastDay.temperature.minimum
+      )}°`;
+    }
+  });
+}
+
 function searchCity(city) {
   let apiKeyUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
   axios.get(apiKeyUrl).then(showWeather);
+
+  let forecastApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(forecastApiUrl).then(showForecast);
 }
 
 function handleSearchSubmit(event) {
@@ -52,3 +92,4 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", fahrenheitTemperature);
 
 searchCity("Cape Town");
+
